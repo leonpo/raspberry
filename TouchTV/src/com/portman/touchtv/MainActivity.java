6933ab00b5fe767c2226088f61256c463626446f
leonpo <leon.portman@gmail.com>
2013-02-24 11:16:19 +0200

    Initial skeleleton

--- TouchTV/src/com/portman/touchtv/MainActivity.java
+++ TouchTV/src/com/portman/touchtv/MainActivity.java
@@ -20,6 +20,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+	static final int NUM_PAGES = 6;
+    public static final String ARG_SECTION_NUMBER = "section_number";
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -72,6 +74,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
             actionBar.addTab(
                     actionBar.newTab()
                             .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            //.setIcon(R.drawable.ic_launcher)
                             .setTabListener(this));
         }
     }
@@ -110,20 +113,40 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
         @Override
         public Fragment getItem(int position) {
+        	Fragment fragment = null;
+        	
             // getItem is called to instantiate the fragment for the given page.
-            // Return a DummySectionFragment (defined as a static inner class
-            // below) with the page number as its lone argument.
-            Fragment fragment = new DummySectionFragment();
-            Bundle args = new Bundle();
-            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+        	switch ( position ) {
+        		case  0:
+        			fragment = new FriendsFragment();
+        			break;
+        		case  1:
+        			fragment = new RemindersFragment();
+        			break;
+        		case  2:
+        			fragment = new EPGFragment();
+        			break;
+        		case  3:
+        			fragment = new UserFragment();
+        			break;
+        		case  4:
+        			fragment = new BrowserFragment();
+        			break;
+        		case  5:
+        			fragment = new SearchFragment();
+        			break;
+        	}
+
+        	Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, position + 1);
             fragment.setArguments(args);
             return fragment;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            // Show 6 total pages.
+            return NUM_PAGES;
         }
 
         @Override
@@ -131,34 +154,124 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
             Locale l = Locale.getDefault();
             switch (position) {
                 case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
+                    return getString(R.string.title_friends).toUpperCase(l);
                 case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
+                    return getString(R.string.title_reminders).toUpperCase(l);
                 case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
+                    return getString(R.string.title_epg).toUpperCase(l);
+                case 3:
+                    return getString(R.string.title_user).toUpperCase(l);
+                case 4:
+                    return getString(R.string.title_browser).toUpperCase(l);
+                case 5:
+                    return getString(R.string.title_search).toUpperCase(l);
             }
             return null;
         }
     }
-
+    
     /**
-     * A dummy fragment representing a section of the app, but that simply
-     * displays dummy text.
+     * Friends fragment
      */
-    public static class DummySectionFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        public DummySectionFragment() {
+    public static class FriendsFragment extends Fragment {
+        
+    	public FriendsFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+    
+    /**
+     * Reminders fragment
+     */
+    public static class RemindersFragment extends Fragment {
+        
+    	public RemindersFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_reminders, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+    
+    /**
+     * EPG fragment
+     */
+    public static class EPGFragment extends Fragment {
+        
+    	public EPGFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_epg, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+    
+    /**
+     * User fragment
+     */
+    public static class UserFragment extends Fragment {
+        
+    	public UserFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_user, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * Browser fragment
+     */
+    public static class BrowserFragment extends Fragment {
+        
+    	public BrowserFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_browser, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+    
+    /**
+     * Search fragment
+     */
+    public static class SearchFragment extends Fragment {
+        
+    	public SearchFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_search, container, false);
             TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
             dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
